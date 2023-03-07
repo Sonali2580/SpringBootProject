f3b1cb997a174c5ca9d792973a7eee665f659d68
Acer <Acer@DESKTOP-9R5NER4>
2023-03-07 18:37:38 +0530

    springboot

---
+++ cofeehouse/src/main/java/coffeehouse/com/CofeehouseApplication.java
@@ -0,0 +1,13 @@
+package coffeehouse.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CofeehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CofeehouseApplication.class, args);
+	}
+
+}
---
+++ cofeehouse/src/test/java/coffeehouse/com/CofeehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package coffeehouse.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofeehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
